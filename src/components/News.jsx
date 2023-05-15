09923bfa8ce76d7bb1a35aed9a05875a0c2eb11e
smuushi <mishih780@gmail.com>
2023-05-15 17:39:41 -0400

    work

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -513,6 +513,159 @@ const News = () => {
                 {/* END MODAL */}
 
               {/* END LIST INNER */}
+            
+              <li
+              data-aos="fade-right"
+              data-aos-duration="1200"
+              data-aos-delay="300"
+            >
+              <div className="list_inner">
+                <div className="image" >
+                  <img src="assets/img/thumbs/4-3.jpg" alt="thumb" />
+                  <a href="https://www.linkedin.com/posts/michael-shih-smuushi_premature-optimization-activity-7059331965912543232-u2yM?utm_source=share&utm_medium=member_desktop">
+
+                  <div
+                    className="main"
+                    style={{
+                      backgroundImage: "url(assets/img/news/nomoat.jpeg)",
+                    }}
+                    ></div>
+                  </a>
+                </div>
+                {/* END IMAGE */}
+
+                <div className="details">
+                  <div className="extra">
+                    <p className="date">
+                      <a href="https://www.linkedin.com/posts/michael-shih-smuushi_google-we-have-no-moat-and-neither-does-activity-7062116822904369152-I4Kk?utm_source=share&utm_medium=member_desktop">We Have No Moat</a>
+                      <span>May 10 2023</span>
+                    </p>
+                  </div>
+
+                  <h3 className="title" >
+                    LinkedIn Post
+                  </h3>
+                  <div className="tokyo_tm_read_more">
+                    <a href="https://www.linkedin.com/posts/michael-shih-smuushi_google-we-have-no-moat-and-neither-does-activity-7062116822904369152-I4Kk?utm_source=share&utm_medium=member_desktop">
+                      <span>Read More</span>
+                    </a>
+                  </div>
+                </div>
+                {/* END DETAILS */}
+
+                {/* START MODAL */}
+                <Modal
+                  isOpen={isOpen3}
+                  onRequestClose={toggleModalThree}
+                  contentLabel="My dialog"
+                  className="mymodal"
+                  overlayClassName="myoverlay"
+                  closeTimeoutMS={500}
+                >
+                  <div className="tokyo_tm_modalbox_news">
+                    <button className="close-modal" onClick={toggleModalThree}>
+                      <img src="assets/img/svg/cancel.svg" alt="close icon" />
+                    </button>
+                    <div className="box_inner">
+                      <div className="description_wrap scrollable">
+                        <div className="image">
+                          <img src="assets/img/thumbs/4-3.jpg" alt="thumb" />
+                          <div
+                            className="main"
+                            style={{
+                              backgroundImage: "url(assets/img/news/3.jpg)",
+                            }}
+                          ></div>
+                        </div>
+                        <div className="details">
+                          <div className="extra">
+                            <p className="date">
+                              By <a href="#">Paola Atkins</a>
+                              <span>15 Feb 2021</span>
+                            </p>
+                          </div>
+                          <h3 className="title">
+                            Why every photographer should shoot film, even in
+                            2021
+                          </h3>
+                        </div>
+                        <div className="main_content ">
+                          <div className="descriptions">
+                            <p className="bigger">
+                              Just because we can't get out and about like we
+                              normally would, doesn’t mean we have to stop
+                              taking pictures. There’s still plenty you can do,
+                              provided you're prepared to use some imagination.
+                              Here are a few ideas to keep you shooting until
+                              normal life resumes.
+                            </p>
+                            <p>
+                              Most photographers love to shoot the unusual, and
+                              you don’t get much more unusual than These
+                              Unprecedented Times. Right now everything counts
+                              as out of the ordinary. There are a number of
+                              remarkable things about these lockdown days that
+                              are worth photographing now so we can remember
+                              them when it is all over.
+                            </p>
+                            <p>
+                              Streets empty that are usually busy are remarkable
+                              and can evoke the sense of historical pictures
+                              from before the invention of the motorcar. Other
+                              things that are different at the moment will be
+                              queues to get into stores and the lines marked out
+                              on the floor to show how far apart we should be.
+                            </p>
+                            <div className="quotebox">
+                              <div className="icon">
+                                <img
+                                  className="svg"
+                                  src="assets/img/svg/quote.svg"
+                                  alt="quore"
+                                />
+                              </div>
+                              <p>
+                                Most photographers find it hard to see
+                                interesting pictures in places in which they are
+                                most familiar. A trip somewhere new seems always
+                                exactly what our photography needed, as shooting
+                                away from home consistently inspires us to new
+                                artistic heights.
+                              </p>
+                            </div>
+                            {/* END QUOTEBOX */}
+                            <p>
+                              Pretend everything is new and that you haven’t
+                              seen it before, and then you will be free to
+                              notice the leading lines, the places where one
+                              edge meets another in delightful geometric
+                              harmony, and how the ordinary things in the
+                              kitchen are transformed when the light is on or
+                              off.
+                            </p>
+                            <p>
+                              The trick here is to look slowly, and then look
+                              again. Take the time to look in detail and to look
+                              at the same thing from different angles, with
+                              different light, long lenses and wide lenses. Then
+                              move to the left a bit. You may never feel the
+                              need to leave the house again.
+                            </p>
+                          </div>
+                          <div className="news_share">
+                            <span>Share:</span>
+                            <Social />
+                            {/* END SCCIAL SHARE */}
+                          </div>
+                        </div>
+                        {/* END MAIN CONTENT */}
+                      </div>
+                    </div>
+                  </div>
+                </Modal>
+              </div>
+              {/* END LIST INNER */}
+            </li>
 
           </ul>
         </div>
